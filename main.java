efe92380269ece5964c9d890f41dee4858a1ee2a
Jashwanth Ramesh <jashwanth1805@gmail.com>
2026-08-19 14:08:37 +0530

    insert-1

--- main.java
+++ main.java
@@ -1,1 +1,6 @@
-
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins!");
+        System.out.println("Java project built successfully.");
+    }
+}
