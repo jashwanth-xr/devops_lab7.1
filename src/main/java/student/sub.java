4fc18cd64526bbece3b1b8a9deca0bb5a1b542ee
Jashwanth Ramesh <jashwanth1805@gmail.com>
2026-08-19 14:54:46 +0530

    init maven proj

---
+++ src/main/java/student/sub.java
@@ -0,0 +1,19 @@
+package student;
+import com.mpatric.mp3agic.*;
+import java.io.IOException;
+import java.nio.file.Path;
+public class sub {
+    public static String getArtist(Path path) {
+        try {
+            Mp3File mp3file = new Mp3File(path.toString());
+            ID3v2 id3v2Tag = mp3file.getId3v2Tag();
+            if (id3v2Tag != null) {
+                String albumArtist = id3v2Tag.getAlbumArtist();
+                return albumArtist != null ? albumArtist : id3v2Tag.getArtist();
+            }
+            return null;
+        } catch (IOException | UnsupportedTagException | InvalidDataException e) {
+            return null;
+        }
+    }
+}
